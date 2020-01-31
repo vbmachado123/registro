9b928095ea89e641f6d7b45a720413a4ea338b4d
vbmachado123 <victorbm00@outlook.pt>
2020-01-31 18:38:56 -0300

    Adicionando Toolbar

--- app/src/main/java/victor/machado/com/br/registro/MainActivity.java
+++ app/src/main/java/victor/machado/com/br/registro/MainActivity.java
@@ -2,12 +2,40 @@ package victor.machado.com.br.registro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Informações gerais
+    private String endereco;
+    private String nome;
+    private String rg;
+    private String cpf;
+    private String celular;
+    private RadioGroup radioGroup;
+    private RadioButton radioProprietario;
+    private RadioButton radioMoradorResponsavel;
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("Registro");
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
